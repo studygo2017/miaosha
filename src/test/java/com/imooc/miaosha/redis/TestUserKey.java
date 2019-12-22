2dc602bb88596af48f37c1d9c5a4861abe57d2e7
HuangFeng <985179541@qq.com>
2019-12-22 14:49:17 +0800

    second

--- src/test/java/com/imooc/miaosha/redis/TestUserKey.java
+++ src/test/java/com/imooc/miaosha/redis/TestUserKey.java
@@ -1,7 +1,9 @@
 package com.imooc.miaosha.redis;
 
+import com.imooc.miaosha.domain.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,11 +11,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class TestUserKey {
 
+    @Autowired
+    RedisService redisService;
+
     @Test
     public void testAddExpiredTime(){
         UserKey userKey = UserKey.getIdKey;
-        userKey.addExpiredTime((int)(0.5*60*60));
+        userKey.addExpiredTime((int)(10));
         int expiredTime = userKey.getExpiredTime();
         System.out.println(expiredTime);
+        redisService.set(userKey,"test",new User(6L,"fv"));
+        User user = redisService.get(userKey, "test", User.class);
+        System.out.println(user);
     }
 }
